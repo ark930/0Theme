690c6d160d74566dd08abfba5d1cd69bfadfcc26
edwin <lark930@gmail.com>
2016-09-29 23:13:29 +0800

    Automate generate users and themes data to database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,13 +11,56 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    $memberships = ['basic', 'pro', 'lifetime'];
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'password' => bcrypt('secret'),
+        'registered' => random_int(0, 1),
+        'register_at' => date('Y-m-d H:i-s'),
+        'membership' => $memberships[array_rand($memberships)],
+        'email_confirm_code' => str_random(24),
+    ];
+});
+
+$factory->define(App\Models\Theme::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'author' => $faker->name,
+    ];
+});
+
+$factory->define(App\Models\ThemeVersion::class, function (Faker\Generator $faker) {
+    return [
+        'author' => $faker->name,
+        'version' => '1.0.1',
+        'description' => $faker->sentence,
+        'logo' => $faker->url,
+        'release_at' => date('Y-m-d H:i-s'),
+        'demo_url' => $faker->url,
+        'lite_url' => $faker->url,
+        'content' => $faker->sentence,
+        'requirement' => $faker->sentence,
+        'premium_store_at' => $faker->url,
+        'premium_store_type' => 'local',
+        'premium_sha1' => $faker->sha1,
+    ];
+});
+
+$factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
+    $type = ['theme_type', 'theme_category'];
+
+    return [
+        'name' => $faker->word,
+        'type' => $type[array_rand($type)],
+    ];
+});
+
+$factory->define(App\Models\ThemeDownload::class, function (Faker\Generator $faker) {
+    return [
+        'download_at' => date('Y-m-d H:i-s'),
+        'ip' => $faker->ipv4,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
+    private $users;
+    private $tags;
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +15,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//        Model::unguard();
+
+        // generate user data
+        $this->users = factory(App\Models\User::class, 5)->create();
+
+        // generate tag data
+        $this->tags = factory(App\Models\Tag::class, 20)->create();
+
+        // generate theme data
+        factory(App\Models\Theme::class, 5)->create()->each(function($theme) {
+            // associate to theme version
+            $theme->versions()->saveMany(factory(App\Models\ThemeVersion::class, random_int(2, 5))->make());
+
+            // create a theme version and associate current version to it
+            $themeVersion = factory(App\Models\ThemeVersion::class)->make();
+            $theme->versions()->save($themeVersion);
+            $theme->currentVersion()->associate($themeVersion);
+            $theme->save();
+
+            // attach tags to theme version
+            $tags = $this->tags;
+            $randomTag = null;
+            for($i = 0; $i < random_int(1, 3); $i++) {
+                $j = random_int(0, $this->tags->count() -1 );
+                $randomTag = $tags->pull($j);
+                $theme->tags()->attach($randomTag);
+            }
+
+            // generate theme download data
+//            $theme->versions()->each(function($themeVersion) {
+//                $themeDownload = factory(App\Models\ThemeDownload::class, random_int(2, 10))->make();
+//
+//                $user = $this->users->random();
+//                $user = \App\Models\User::find($user['id']);
+////                echo $user['id'];
+////                $user->downloads()->save($themeDownload);
+//                $themeDownload->user()->associate($user);
+////                $themeDownload->save();
+////                $themeVersion->downloads()->saveMany($themeDownload);
+//            });
+        });
+
+
+//        Model::reguard();
+
     }
 }
